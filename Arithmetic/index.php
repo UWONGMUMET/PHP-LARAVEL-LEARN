652cd6b1d5330f0d7b3ff9b64fe4735efdff93d5
WONG ELEK <111182255+UWONGMUMET@users.noreply.github.com>
2024-07-24 01:23:38 +0700

    Add files via upload

---
+++ Arithmetic/index.php
@@ -0,0 +1,52 @@
+<?php
+    // Arithmetic operators
+    $x = 10;
+    $y = 3;
+
+    // Addition
+    $z = $x + $y;
+    echo "$x + $y = $z";
+    echo "<br>";
+
+    // Subtraction
+    $z = $x - $y;
+    echo "$x - $y = $z";
+    echo "<br>";
+
+    // Multiplication
+    $z = $x * $y;
+    echo "$x * $y = $z";
+    echo "<br>";
+
+    // Division
+    $z = $x / $y;
+    echo "$x / $y = $z";
+    echo "<br>";
+
+    // Exponentiation
+    $z = $x ** $y;
+    echo "$x ** $y = $z";
+    echo "<br>";
+
+    // Modulus
+    $z = $x % $y;
+    echo "$x % $y = $z";
+    echo "<br>";
+
+    // Increment/Decrement operators
+
+    $counter = 0;
+    echo "Initial counter: $counter<br>";
+    
+    // Increment by 2
+    $counter += 2;
+    echo "Counter after incrementing by 2: $counter<br>";
+
+    // Decrement by 1
+    $counter--;
+    echo "Counter after decrementing by 1: $counter<br>";
+
+    // Increment by 1
+    $counter++;
+    echo "Counter after incrementing by 1: $counter<br>";
+?>
