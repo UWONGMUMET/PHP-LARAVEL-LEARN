74be2feddf4c3bd6086a431664a93a3fffe86a7a
WONG ELEK <111182255+UWONGMUMET@users.noreply.github.com>
2024-08-17 23:09:44 +0700

    Add files via upload

---
+++ Arrays/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $foods = array("appple", "orange", "banana", "coconut");
+
+    // $foods[0] = "pineapple";
+    // array_push($foods, "pineapple");
+    // array_pop($foods);
+    // array_shift($foods);
+    // $foods = array_reverse($foods);
+    // echo count($foods);
+
+    foreach($foods as $food) {
+        echo $food . "<br>";
+    }
+
+?>
